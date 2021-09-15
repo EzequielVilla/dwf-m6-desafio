3e27f09fd9d96cb19aa7b13297a492f8f809bb9b
EzequielVilla <ezequiel.n.villa@gmail.com>
2021-09-15 01:30:26 -0300

    env

--- client/state.ts
+++ client/state.ts
@@ -1,7 +1,7 @@
 import {rtdb} from "./rtdb"
 
-const API_BASE_URL = "http://localhost:3000";
-console.log(process.env.HOST, 'process.env.HOST');
+const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3000";
+console.log(process.env.API_BASE_URL, 'process.env.HOST');
 
 
 
